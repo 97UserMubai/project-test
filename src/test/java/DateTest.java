d87f910497922b56f8ae25161361caf527627bdb
wangbaitao <1610386885@qq.com>
2020-09-30 09:27:05 +0800

    2020-09-30测试

--- src/test/java/DateTest.java
+++ src/test/java/DateTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author wangbaitao
@@ -25,6 +26,15 @@ public class DateTest {
         long compareSeconds = localDateTime.toEpochSecond(OffsetDateTime.now().getOffset())
                 - localDateTime1.toEpochSecond(OffsetDateTime.now().getOffset());
         System.out.println(compareSeconds);
+    }
+
+    @Test
+    public void congraduation() {
+        //番薯纪念日
+        LocalDate loveDate = LocalDate.parse("2017-01-10", DateTimeFormatter.ISO_DATE);
+        System.out.println("恋爱那天：" + loveDate.toString());
+        System.out.println("今天是：" + LocalDate.now().toString());
+        System.out.println("王狗子和沈咸鱼的恋爱已经" + (LocalDate.now().toEpochDay() - loveDate.toEpochDay()) + "天了");
     }
 
 }
