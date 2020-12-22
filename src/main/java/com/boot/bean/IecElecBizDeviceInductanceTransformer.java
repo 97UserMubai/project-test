15c25688f64f16424740bb216a549ce988538eff
wangbaitao <wangbaitao@listener.com.cn>
2020-12-22 17:37:27 +0800

    测试提交1222

---
+++ src/main/java/com/boot/bean/IecElecBizDeviceInductanceTransformer.java
@@ -0,0 +1,132 @@
+package com.boot.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 业扩-现场勘查-电流互感器/电压互感器
+ * </p>
+ *
+ * @author huanghebin
+ * @since 2020-12-15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("iec_elec_biz_device_inductance_transformer")
+@Builder
+public class IecElecBizDeviceInductanceTransformer implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.ASSIGN_UUID)
+    private String id;
+
+    /**
+     * 运行互感器标识
+     */
+    private String sign;
+
+    /**
+     * 变更标志（新装、拆除、更换前、更换后、暂停、恢复、抄表前、抄表后、移入、移出、换抽头前、换抽头后）
+     */
+    private String changeSign;
+
+    /**
+     * 计量点编号
+     */
+    private String calculateNumber;
+
+    /**
+     * 拆装标志（0否；1是）
+     */
+    private Boolean installSign;
+
+    /**
+     * 产权归属代码（01 局属、02 用户）
+     */
+    private String ownershipCode;
+
+    /**
+     * 执行班组（组织部门）
+     */
+    private String executor;
+
+    /**
+     * 资产编号
+     */
+    private String code;
+
+    /**
+     * 设备类型代码（电流互感器；电压互感器；组合式互感器）
+     */
+    private String typeCode;
+
+    /**
+     * 额定电压（220V
+3×220/380V
+3×57.7/100V
+3×380V
+3×100V）
+     */
+    private String voltage;
+
+    /**
+     * 相别（A相
+B相
+C相
+AB相
+AC相
+BC相
+AB相+ BC相）
+     */
+    private String phase;
+
+    /**
+     * TA变比
+     */
+    private String rateTa;
+
+    /**
+     * TV变比
+     */
+    private String rateTv;
+
+    /**
+     * TA准确度等级
+     */
+    private String precisionTa;
+
+    /**
+     * TV准确度等级
+     */
+    private String precisionTv;
+
+    /**
+     * 类型（0-TV；1-TA）
+     */
+    private Boolean type;
+
+    /**
+     * 工单号
+     */
+    private String orderCode;
+
+    /**
+     * 用电户编号
+     */
+    private String elecUserCode;
+
+    /**
+     * 线路图元件id
+     */
+    private String diagramId;
+}
---
+++ src/main/java/com/boot/bean/IecElecBizDeviceLoad.java
@@ -0,0 +1,187 @@
+package com.boot.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * @author wangbaitao
+ * @version 1.0.0
+ * @Date 2020/10/14
+ * <h></h>
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("iec_elec_biz_device_load")
+@Builder
+public class IecElecBizDeviceLoad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.ASSIGN_UUID)
+    private String id;
+
+    /**
+     * 工作单编号
+     */
+    private String orderCode;
+
+    /**
+     * 运行终端标识
+     */
+    private String terminalSign;
+
+    /**
+     * 变更标志
+     */
+    private String changeSign;
+
+    /**
+     * 计量点编号
+     */
+    private String calculateNumber;
+
+    /**
+     * 拆装标志
+     */
+    private Boolean assembleSign;
+
+    /**
+     * 安装地址
+     */
+    private String installationAddress;
+
+    /**
+     * 产权归属（01 局属、02 用户）
+     */
+    private String propertyOwnership;
+
+    /**
+     * 执行班组
+     */
+    private String executeTeam;
+
+    /**
+     * 资产编号
+     */
+    private String propertyNumber;
+
+    /**
+     * 设备类型
+     */
+    private String deviceType;
+
+    /**
+     * 额定电压
+     */
+    private String nominalVoltage;
+
+    /**
+     * 标定电流
+     */
+    private String ratedCurrent;
+
+    /**
+     * 通讯方式（红外通信
+     RS485通信
+     载波通信
+     无线电通信
+     GPRS通信
+     CDMA通信
+     光纤通信
+     WIFI通信
+     蓝牙通信
+     RJ-11
+     RJ-45
+     RS232）
+     */
+    private String communicationMode;
+
+    /**
+     * 通讯规约（DL/T645
+     IEC1107
+     GB/T 19897.1-2005（IEC62056-21）
+     DL/T698）
+     */
+    private String communicationProtocol;
+
+    /**
+     * 用电户编号
+     */
+    private String elecUserCode;
+
+    /**
+     * 线路图元件id
+     */
+    private String diagramId;
+
+    /**
+     * 电能表标识（关联电能表）
+     */
+    private String meterSign;
+
+    /**
+     * 电能表资产编号
+     */
+    private String meterCode;
+
+    /**
+     * 负荷终端通讯地址
+     */
+    private String communicationAddress;
+
+    /**
+     * 采集对象通讯地址
+     */
+    private String collectionAddress;
+
+    /**
+     * 电能表设备类型
+     */
+    private String meterType;
+
+    /**
+     * 采集类型
+     */
+    private String collectionType;
+
+    /**
+     * 采集对象通讯方式（红外通信
+     * RS485通信
+     * 载波通信
+     * 无线电通信
+     * GPRS通信
+     * CDMA通信
+     * 光纤通信
+     * WIFI通信
+     * 蓝牙通信
+     * RJ-11
+     * RJ-45
+     * RS232）
+     */
+    private String collectionWay;
+
+    /**
+     * 采集对象通讯规约
+     */
+    private String collectionProtocol;
+
+    /**
+     * 测量点号
+     */
+    private String testPoint;
+
+    /**
+     * 变更日期
+     */
+    private LocalDate changeDate;
+
+}
